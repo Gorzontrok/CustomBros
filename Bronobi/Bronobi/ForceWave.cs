6bbe1517b98bf4ae992428d0b5ab077c3c1a9082
Gorzon38 <64686176+Gorzontrok@users.noreply.github.com>
2024-01-15 15:06:26 +0100

    Update to last bromaker

--- Bronobi/Bronobi/ForceWave.cs
+++ Bronobi/Bronobi/ForceWave.cs
@@ -8,8 +8,11 @@ namespace BronobiMod
     public class ForceWave : CharacterAbility
     {
         public Texture2D forceWaveTexture;
-        public override void All()
+        public override void All(string method, params object[] objects)
         {
+            if (method == "Update" || method == "Start" || method == "Awake")
+                return;
+
             var forceWave = new GameObject("BronobiForceWave").AddComponent<BronobiForceWave>();
             forceWave.transform.position = owner.transform.position;
             forceWave.Setup(forceWaveTexture);
