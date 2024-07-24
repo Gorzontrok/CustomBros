50b18e7fb2ff28d1c1237f6b3e47c1ae97bb1971
Gorzontrok <64686176+Gorzontrok@users.noreply.github.com>
2024-07-24 19:44:30 +0200

    Update MindControlWave.cs

--- Bronobi/Bronobi/MindControlWave.cs
+++ Bronobi/Bronobi/MindControlWave.cs
@@ -1,34 +1,17 @@
 ﻿using BroMakerLib.Loggers;
+using RocketLib;
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace BronobiMod
 {
     public class MindControlWave : FlameWallExplosion
     {
-        void Awake()
-        {
-            assasinateUnits = true; // make sure that 'TryAssassinateUnits(float,float,int,int,int)' is called
-            damageDoodads = false;
-            knockUnits = false;
-            blindUnits = false;
+        public TestVanDammeAnim Character
+        { get => firedBy as  TestVanDammeAnim;}
 
-            rotateExplosionSprite = true;
-
-            maxCollumns = 32;
-            maxRows = 3;
-            totalExplosions = 60;
-            explosionRate = 0.04f;
-        }
-
-        protected override void TryAssassinateUnits(float x, float y, int xRange, int yRange, int playerNum)
-        {
-            Mook closestMook = Map.GetNearbyMook((float)xRange, (float)yRange, x, y, (forceDirection == DirectionEnum.Left ? -1 : 1), true);
-            if (closestMook)
-            {
-                closestMook.playerNum = playerNum;
-            }
-        }
+        [SerializeField] protected float _mindControlTimeInSecond = 2.0f;
 
         public void Setup(Texture2D wavePointTexture)
         {
@@ -61,5 +44,46 @@ namespace BronobiMod
                 BMLogger.Log($"[{typeof(MindControlWave)}] {ex}");
             }
         }
+
+        protected virtual void Awake()
+        {
+            assasinateUnits = true; // make sure that 'TryAssassinateUnits(float,float,int,int,int)' is called
+            damageDoodads = false;
+            knockUnits = false;
+            blindUnits = false;
+
+            rotateExplosionSprite = true;
+
+            maxCollumns = 32;
+            maxRows = 3;
+            totalExplosions = 60;
+            explosionRate = 0.04f;
+        }
+
+        protected override void TryAssassinateUnits(float x, float y, int xRange, int yRange, int playerNum)
+        {
+            Mook closestMook = Map.GetNearbyMook((float)xRange, (float)yRange, x, y, (forceDirection == DirectionEnum.Left ? -1 : 1), false);
+            if (closestMook != null)
+            {
+                ControlMook(closestMook);
+            }
+        }
+
+        protected virtual void ControlMook(Mook mook)
+        {
+            mook.playerNum = playerNum;
+            mook.gameObject.SetOwnerNetworked(Character.Owner);
+            mook.SetReviveSource(Character);
+            mook.SetIsZombie(true);
+        }
+
+        private IEnumerator StopControlMookCoroutine(Mook mook, int previousPlayerNum)
+        {
+            yield return new WaitForSeconds(_mindControlTimeInSecond);
+
+            mook.playerNum = previousPlayerNum;
+            mook.Blind(0.0f);
+        }
+
     }
 }
