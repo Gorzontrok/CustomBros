5ee61061b5e3c93126fd5f2a8055dfb3fe2c3140
Gorzontrok <64686176+Gorzontrok@users.noreply.github.com>
2025-10-09 19:07:51 +0200

    Cleaning

--- TonyBrotana/TonyBrotana.cs
+++ TonyBrotana/TonyBrotana.cs
@@ -1,24 +1,32 @@
 ﻿using BroMakerLib;
 using BroMakerLib.CustomObjects.Bros;
 using RocketLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using UnityEngine;
 
 namespace TonyBrotanaMod
 {
     [HeroPreset(nameof(TonyBrotana), HeroType.Rambro)]
     public class TonyBrotana : CustomHero
     {
-        protected int _fireCount = 0;
-        protected float _rocketDelay = 0.5f;
-        protected bool _willFireRocket = false;
-        protected Projectile _rocket;
+        public Texture2D RocketTexture
+        {
+            get
+            {
+                if (_rocketTexture == null)
+                    _rocketTexture = ResourcesController.GetTexture(DirectoryPath, "TonyBrotana_Special.png");
+                return _rocketTexture;
+            }
+        }
+        private Texture2D _rocketTexture;
+
+        protected int fireCount = 0;
+        protected float rocketDelay = 0.25f;
+        protected bool willFireRocket = false;
+        protected Projectile rocket;
 
         protected override void Awake()
         {
-            _rocket = HeroController.GetHeroPrefab(HeroType.Brommando).projectile;
+            rocket = HeroController.GetHeroPrefab(HeroType.Brommando).projectile;
             base.Awake();
         }
 
@@ -30,29 +38,29 @@ namespace TonyBrotanaMod
         protected override void FireWeapon(float x, float y, float xSpeed, float ySpeed)
         {
             base.FireWeapon(x, y, xSpeed, ySpeed);
-            if (_willFireRocket)
+            if (willFireRocket)
             {
-                _willFireRocket = false;
-                fireDelay = _rocketDelay;
+                willFireRocket = false;
+                fireDelay = rocketDelay;
                 return;
             }
-            _fireCount++;
-            if (_fireCount >= 3)
+            fireCount++;
+            if (fireCount >= 3)
             {
-                _fireCount = 0;
-                fireDelay = _rocketDelay;
-                _willFireRocket = true;
+                fireCount = 0;
+                fireDelay = rocketDelay;
+                willFireRocket = true;
             }
 
         }
 
         protected override Projectile SpawnPrimaryProjectile(Projectile projectilePrefab, float x, float y, float xSpeed, float ySpeed)
         {
-            if (_willFireRocket)
+            if (willFireRocket)
             {
-                Projectile rocket = ProjectileController.SpawnProjectileLocally(_rocket, this, x, y, xSpeed, ySpeed, base.playerNum);
-                rocket.SetRendererTexture(ResourcesController.GetTexture(DirectoryPath, "TonyBrotana_Special.png"));
-                return rocket;
+                Projectile rocketSpawned = ProjectileController.SpawnProjectileLocally(rocket, this, x, y, xSpeed, ySpeed, playerNum);
+                rocketSpawned.SetRendererTexture(RocketTexture);
+                return rocketSpawned;
             }
             Projectile proj = base.SpawnPrimaryProjectile(projectilePrefab, x, y, xSpeed, ySpeed);
             proj.damage = 3;
